407d4de454125a569b21359b75e425fdb368b0ab
Ezequiel <pezequiel255@gmail.com>
2025-03-13 20:14:28 -0300

    segunda parte

--- arquivo.py
+++ arquivo.py
@@ -3,3 +3,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+    def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+def main():
+    print("Bem-vindo ao conversor de temperaturas!")
+    temp_c = float(input("Digite a temperatura em Celsius: "))
+    temp_f = celsius_to_fahrenheit(temp_c)
+    print(f"{temp_c}°C é igual a {temp_f}°F")
+
+if __name__ == "__main__":
+    main()
